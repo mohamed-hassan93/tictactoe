17a4ce628a41932e363894e661f0b4e6d73daf66
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-05-10 18:31:22 -0400

    updated code

--- js/main.js
+++ js/main.js
@@ -41,3 +41,45 @@ cells.forEach(function(cell){
 )
 
 
+// class Game{
+//   constructor(currentPlayer, nextPlayer){
+//     this._currentPlayer = currentPlayer
+//     this._nextPlayer = nextPlayer
+  
+//   }
+//   get currentPlayer(){
+//     return this._currentPlayer
+//   }
+//   get nexttPlayer(){
+//     return this._nextPlayer
+//   }
+//   checkForWinner (){
+//     winningCombination.forEach(function(combination){
+//       let check = combination.every(index =>  cells[index].innerText.trim() == currentPlayer)
+//       if(check == true ){
+//         document.querySelector('.result').innerHTML = `Winner: Player ${currentPlayer}`
+//       }else{
+//         return
+//       }
+//     })
+    
+//     cells.forEach(function(cell){
+//       cell.addEventListener('click', function(){
+//         if(cell.innerText.trim() == ''){
+//           cell.innerText = currentPlayer;
+//           checkForWinner()
+//           currentPlayer = currentPlayer == this._currentPlayer ? this._nextPlayer : this._currentPlayer
+//         }else{
+//           return
+//         }
+        
+//       })
+//     }
+//     )
+  
+  
+//   }
+
+// }
+
+// let game = new Game('X', 'O')
